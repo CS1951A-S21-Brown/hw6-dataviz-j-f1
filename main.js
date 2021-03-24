41cb50b198b8d4d7d1a9096d0e2ede61a582c466
Jed Fox <git@jedfox.com>
2021-03-24 17:33:09 -0400

    Finish runtime graph

--- main.js
+++ main.js
@@ -52,7 +52,9 @@ function constructRuntimeByYear(movies, width) {
     .groups(movies, (d) => d.release_year)
     .map(([year, movies]) => ({
       year,
+      minRuntime: d3.min(movies, (d) => d.minutes),
       runtime: d3.mean(movies, (d) => d.minutes),
+      maxRuntime: d3.max(movies, (d) => d.minutes),
     }))
     .sort((a, b) => a.year - b.year);
 
@@ -61,15 +63,30 @@ function constructRuntimeByYear(movies, width) {
     [0, width]
   );
   const y = d3
-    .scaleLinear([d3.max(data, (d) => d.runtime), 0], [0, height])
+    .scaleLinear([d3.max(movies, (d) => d.minutes), 0], [0, height])
     .nice();
+
+  svg
+    .append("text")
+    .attr("transform", `translate(${width / 2}, -25)`)
+    .attr("font-size", 20)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .text("Average runtime by year");
+  svg
+    .append("text")
+    .attr("transform", `translate(${width / 2}, -5)`)
+    .attr("text-anchor", "middle")
+    .text("gray region depicts full range of runtimes for the given year");
 
   svg
     .append("text")
     .attr("transform", `translate(-50, ${height / 2}) rotate(-90)`)
     .attr("text-anchor", "middle")
     .text("Average runtime (minutes)");
+
   svg.append("g").call(d3.axisLeft(y));
+
   svg
     .append("g")
     .attr("transform", `translate(0, ${height})`)
@@ -80,4 +97,30 @@ function constructRuntimeByYear(movies, width) {
     .append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x).ticks(10, "d").tickSize(10));
+
+  const area = d3
+    .area()
+    .x((d) => x(d.year))
+    .y0((d) => y(d.minRuntime))
+    .y1((d) => y(d.maxRuntime))
+    .curve(d3.curveCardinal);
+  svg
+    .append("path")
+    .attr("stroke-width", "2")
+    .attr("fill", "black")
+    .attr("opacity", 0.25)
+    .attr("d", area(data));
+
+  const line = d3
+    .line()
+    .x((d) => x(d.year))
+    .y((d) => y(d.runtime))
+    .curve(d3.curveCardinal);
+  svg
+    .append("path")
+    .attr("fill", "none")
+    .attr("stroke-width", "3")
+    .attr("stroke", "black")
+    .attr("stroke-linejoin", "round")
+    .attr("d", line(data));
 }
