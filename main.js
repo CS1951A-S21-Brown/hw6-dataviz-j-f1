5d6ae576f42bd3ba89bc88f35122668dc2a391a9
Jed Fox <git@jedfox.com>
2021-03-24 16:53:20 -0400

    read in data

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ let graph_2_width = MAX_WIDTH / 2 - 10,
   graph_2_height = 275;
 let graph_3_width = MAX_WIDTH / 2,
   graph_3_height = 575;
+
+const delist = (s) => s.split(", ").filter(Boolean);
+const parseDate = d3.timeParse("%B %-d, %Y");
+(async () => {
+  const data = await d3.csv("data/netflix.csv", (d) => ({
+    cast: delist(d.cast),
+    country: delist(d.country),
+    date_added: parseDate(d.date_added),
+    description: d.description,
+    director: delist(d.director),
+    duration: d.duration,
+    listed_in: delist(d.listed_in),
+    rating: d.rating,
+    release_year: +d.release_year,
+    title: d.title,
+    type: d.type,
+  }));
+
+  const movies = data
+    .filter((d) => d.type === "Movie")
+    .map(({ duration, ...m }) => ({ ...m, minutes: parseInt(duration) }));
+  const shows = data
+    .filter((d) => d.type === "TV Show")
+    .map(({ duration, ...m }) => ({ ...m, seasons: parseInt(duration) }));
+
+  debugger;
+})();
